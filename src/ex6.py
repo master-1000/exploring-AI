52362d92dfb17bc719579a5d876a0ef8962c5e71
Carlos <carloseduardoleal10@gmail.com>
2023-04-24 21:23:51 -0300

    atualização 1.1

--- src/ex6.py
+++ src/ex6.py
@@ -5,7 +5,7 @@ def generate_password(length):
     characters = string.ascii_letters + string.digits + string.punctuation
     
     password = ''.join(random.choice(characters) for i in range(length))
-    
+       
     return password
 
 password_length = int(input("Quantos caracteres deve ter a senha? "))
